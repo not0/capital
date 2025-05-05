ef6cfeb872ec64f868c8be4ba9e08f201a0afa23
not0 <anoato@gmail.com>
2025-05-05 17:33:52 +0900

    カスタムドメイン対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://not0.github.io",
-  base: "capital",
+  site: "https://not0.xyz",
 });
